ce31274310d49e7a95ee539f0e9e8cf29c40be13
Ivelina Plocheva <ivelina.plocheva@newredo.com>
2023-01-27 08:20:37 +0000

    Add a delete action.

--- routes/list.js
+++ routes/list.js
@@ -2,18 +2,29 @@
 
 const express = require('express');
 
-const QRcode = require('../qr-code-schema.js'); // import the schema
+const QRCode = require('../qr-code-schema.js'); // import the schema
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const qrCodes = await QRcode.find();
+        const qrCodes = await QRCode.find();
         res.render('list', { qrCodes });
     } catch (err) {
         console.log(err);
         res.status(500).send('Error retrieving QR codes');
     }
+});
+
+// Delete a QR code
+router.post('/:id', (req, res) => {
+    QRCode.findByIdAndDelete(req.params.id, (err) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.redirect('/list');
+        }
+    });
 });
 
 module.exports = router;
