007f2fd57b1d2509b69b87c27435c2fe1f0afa07
Ivelina Plocheva <ivelina.plocheva@newredo.com>
2023-08-20 13:09:56 +0100

    Add export to CSV option in the QR listing page.

--- routes/list.js
+++ routes/list.js
@@ -1,14 +1,27 @@
 "use strict";
 
 const express = require('express');
-
-const QRCode = require('../qr-code-schema.js'); // import the schema
+const Papa = require('papaparse');
+const QRCode = require('../qr-code-schema.js');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
         const qrCodes = await QRCode.find();
+
+        // Check if the user wants to export to CSV
+        if (req.query.export === 'csv') {
+            const csv = Papa.unparse(qrCodes.map(code => ({
+                message: code.message,
+                qrCode: code.qrCode
+            })));
+
+            res.setHeader('Content-Type', 'text/csv');
+            res.setHeader('Content-Disposition', 'attachment; filename=qr-codes.csv');
+            return res.send(csv);
+        }
+
         res.render('list', { qrCodes });
     } catch (err) {
         console.log(err);
@@ -26,5 +39,6 @@ router.post('/:id', (req, res) => {
         }
     });
 });
+
 
 module.exports = router;
